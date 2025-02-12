7eaa93edeef70e687b7237510d7bf8888e35b6cd
duneraccoon <benjamincsherro@hotmail.com>
2025-02-12 13:43:24 +1100

    Bump version to 0.1.2 and update Pyright configuration to suppress general type issue reports

--- employment_hero_sdk/__version__.py
+++ employment_hero_sdk/__version__.py
@@ -4,7 +4,7 @@ EmploymentHero SDK
 A framework-agnostic Python SDK for interacting with the Employment Hero API.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 
 from .client import EmploymentHeroClient
 from .exceptions import EmploymentHeroAPIError
